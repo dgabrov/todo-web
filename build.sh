c34f5dbf55db959a1b4870a5d2099c137db95172
Daniel Gabroveanu <dgabrov@cbox.info>
2022-01-17 14:49:34 -0500

    put build.sh in the right place

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+export NODE_OPTIONS=--openssl-legacy-provider
 cd application
+echo before npm install
 npm install
+echo npm install proceeded successfully
+echo will run build
 npm run build
+echo run build successfully done
 node -v
 npm -v
