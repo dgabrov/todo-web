aa2da99eb8e686772a4cf7ac37fd4bd0ac95e72e
Daniel Gabroveanu <dgabrov@cbox.info>
2022-01-17 14:44:52 -0500

    put build.sh in the right place

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-image_name=node:12.21.0-alpine3.10
-
-echo docker run \
-    -v /home/dgabrov/IdeaProjects/todo-2-react/application:/application \
-    -w "/application" \
-    -u "$(id -u):$(id -g)" \
-    ${image_name} \
-    /bin/sh -c "npm install && npm run build"
-
+#!/bin/sh
+cd application
+npm install
+npm run build
+node -v
+npm -v
